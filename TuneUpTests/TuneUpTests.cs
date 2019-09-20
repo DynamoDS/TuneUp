ea10951de6885bc40ffe227295c9bcd6dcb10aa9
Scott Mitchell <scott.a.mitchell13@gmail.com>
2019-09-20 00:54:15 -0400

    Add TuneUpTests project

---
+++ TuneUpTests/TuneUpTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using SystemTestServices;
+
+using Dynamo.Models;
+using Dynamo.Graph.Workspaces;
+
+
+namespace TuneUpTests
+{
+    public class TuneUpTests : SystemTestBase
+    {
+        [Test, RequiresSTA]
+        public void TestMethod1()
+        {
+            var homespace = Model.CurrentWorkspace as HomeWorkspaceModel;
+        }
+    }
+}
