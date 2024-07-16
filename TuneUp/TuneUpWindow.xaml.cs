5ec3826178bf22eff84a677cf4b3fe7854ebb5e5
Ivo Petrov <48355182+ivaylo-matov@users.noreply.github.com>
2024-07-16 15:57:32 +0100

    node centering only on user's action (#44)
    isUserInitiatedSelection to flag if the nodes in DataGrid are selected by the user

--- TuneUp/TuneUpWindow.xaml.cs
+++ TuneUp/TuneUpWindow.xaml.cs
@@ -28,6 +28,12 @@ namespace TuneUp
         private string uniqueId;
 
         /// <summary>
+        /// A flag indicating whether the current selection change in the DataGrid 
+        /// is initiated by the user (true) or programmatically (false).
+        /// </summary>
+        private bool isUserInitiatedSelection = false;
+
+        /// <summary>
         /// Since there is no API for height offset comparing to
         /// DynamoWindow height. Define it as static for now.
         /// </summary>
@@ -58,6 +64,8 @@ namespace TuneUp
 
         private void NodeAnalysisTable_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (!isUserInitiatedSelection) return;
+
             // Get NodeModel(s) that correspond to selected row(s)
             var selectedNodes = new List<NodeModel>();
             foreach (var item in e.AddedItems)
@@ -79,6 +87,24 @@ namespace TuneUp
                 // Focus on selected
                 viewModelCommandExecutive.FindByIdCommand(selectedNodes.First().GUID.ToString());
             }
+        }
+
+        /// <summary>
+        /// Handles the PreviewMouseDown event for the NodeAnalysisTable DataGrid.
+        /// Sets a flag to indicate that the selection change is user-initiated.
+        /// </summary>
+        private void NodeAnalysisTable_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            isUserInitiatedSelection = true;
+        }
+
+        /// <summary>
+        /// Handles the MouseLeave event for the NodeAnalysisTable DataGrid.
+        /// Resets the flag to indicate that the selection change is no longer user-initiated.
+        /// </summary>
+        private void NodeAnalysisTable_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            isUserInitiatedSelection = false;
         }
 
         internal void Dispose()
